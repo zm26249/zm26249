ae705bf38dec5da81b4ccb0f373927c6883dc081
zm26249 <zm26249@josai.ac.jp>
2026-06-29 11:35:34 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -5,3 +5,9 @@ alert(Math.sqrt(1+1));
 alert(Math.sqrt(2)+Math.sqrt(3));
 alert(1/0);
 alert(0/0);
+
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+Number('3'));
+alert('2'+'3');
+alert(Number('2')+3);
